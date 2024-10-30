d133ca010598ff3c805104c18405bbde0eda72fd
hamidzone <muh.hamiddd@gmail.com>
2024-10-30 15:34:37 +0700

    Tutorial 6 Done

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/menu.dart'; // Import MyHomePage dari menu.dart
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple,
+        ).copyWith(secondary: Colors.deepPurple[400]),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(), // Memanggil MyHomePage dari menu.dart
+);
+}
+}
